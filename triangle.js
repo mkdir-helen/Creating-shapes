d80b88ee017f9529c0f77eb95ad76e7a4a528519
mkdir-helen <experiment.helen@gmail.com>
2018-09-27 17:09:47 -0400

    created a function and added potential variables

--- triangle.js
+++ triangle.js
@@ -1,7 +1,17 @@
 //I want to build a cone in javascript
-var height = 9;
-var num = parseInt(2*height - 1);
-var init_num = num;
+
+// var height = 9;
+// var num = parseInt(2*height - 1);
+// var init_num;
+
+function cone(){
+    var userInput = document.getElementById('demo').value
+    var num = parseInt(2*userInput - 1);
+    init_num = num;
+
+    
+}
+
 while (num > 0){
     if(num % 2 == 1){
         if (init_num == num){
