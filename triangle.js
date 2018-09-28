a43a29acf3c8a20358a268b4b623e733564bee9b
mkdir-helen <experiment.helen@gmail.com>
2018-09-28 19:26:52 -0400

    put everything in function

--- triangle.js
+++ triangle.js
@@ -8,36 +8,36 @@ function cone(){
     var userInput = document.getElementById('demo').value
     var num = parseInt(2*userInput - 1);
     init_num = num;
-
+    while (num > 0){
+        if(num % 2 == 1){
+            if (init_num == num){
+                var a_num = num;
+                while(a_num > 0){
+                    // document.getElementById("demo").innerHTML = 5 + 6;
+                    document.getElementById('demo').innerHTML = '*';
+                    a_num -= 1;
+                }
+            }else{
+                var b_num = num;
+                var x_num = num;
+                while(b_num >= 0){
+                    if(b_num > b_num - x_num){
+                        while(x_num > 0){
+                            document.getElementById('demo').innerHTML = ' ';
+                            b_num -= 1;
+                        }
+                    }else{
+                        document.getElementById('demo').innerHTML = ' ';
+                    }
+                    b_num -= 1;
+                }
+    
+            }
+        }
+        num -= 1;
+    }
     
 }
 
-while (num > 0){
-    if(num % 2 == 1){
-        if (init_num == num){
-            var a_num = num;
-            while(a_num > 0){
-                // document.getElementById("demo").innerHTML = 5 + 6;
-                document.getElementById('demo').innerHTML = '*';
-                a_num -= 1;
-            }
-        }else{
-            var b_num = num;
-            var x_num = num;
-            while(b_num >= 0){
-                if(b_num > b_num - x_num){
-                    while(x_num > 0){
-                        document.getElementById('demo').innerHTML = ' ';
-                        b_num -= 1;
-                    }
-                }else{
-                    document.getElementById('demo').innerHTML = ' ';
-                }
-                b_num -= 1;
-            }
 
-        }
-    }
-    num -= 1;
-}
 
