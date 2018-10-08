105c00ac8bc306fcaf9320ffe4afa6406cfd687c
mkdir-helen <experiment.helen@gmail.com>
2018-10-08 18:42:37 -0400

    Made the selection workable

--- triangle.js
+++ triangle.js
@@ -1,39 +1,46 @@
 //I want to build a cone in javascript
 
+function shapes(){
+  let symbol = document.getElementById('mySelect').value;
+  // let symbol = options[selectedIndex].id;
+  console.log(symbol);
+};
 
-
-function cone_width(height){
-    return parseInt(2*height-1);
-  };
-  let width, oWidth;
-  width = oWidth = cone_width(5);
-  let firstLine = '';
+// function cone_width(height){
+//     return parseInt(2*height-1);
+//   };
+// function make_cone(){
+//   // userInput = document.getElementById.value
+// };  
+//   let width, oWidth;
+//   width = oWidth = cone_width(5);
+//   let firstLine = '';
   
-  for(let num=oWidth; num > 0; num--){
-    if(width == oWidth){
-      for(let num=width; num > 0; num--){
-        firstLine += '*';
-      };
-      console.log(firstLine+ '\n');
-      width--;
-    }else{
-      if(width % 2 === 1){
-        let nextLine = '';
-        let spaces = parseInt((oWidth - width)/2);
-        for(let num=spaces; num>0; num--){
-            nextLine += '1';
+//   for(let num=oWidth; num > 0; num--){
+//     if(width == oWidth){
+//       for(let num=width; num > 0; num--){
+//         firstLine += '*';
+//       };
+//       console.log(firstLine+ '\n');
+//       width--;
+//     }else{
+//       if(width % 2 === 1){
+//         let nextLine = '';
+//         let spaces = parseInt((oWidth - width)/2);
+//         for(let num=spaces; num>0; num--){
+//             nextLine += '1';
           
-        };
-        for(let num=width; num > 0; num--){
-            nextLine += '*';
-        };
-        console.log(nextLine+'\n');
-        width--;
-      }else{
-          width--;
-      };
-    };
-  };
+//         };
+//         for(let num=width; num > 0; num--){
+//             nextLine += '*';
+//         };
+//         console.log(nextLine+'\n');
+//         width--;
+//       }else{
+//           width--;
+//       };
+//     };
+//   };
 
 
 
