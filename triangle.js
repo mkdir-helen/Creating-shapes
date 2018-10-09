a57695d064d1ecce4804d4122122a87f5dc3d190
mkdir-helen <experiment.helen@gmail.com>
2018-10-09 19:03:25 -0400

    Put the code into one function

--- triangle.js
+++ triangle.js
@@ -1,46 +1,45 @@
 //I want to build a cone in javascript
 
-function shapes(){
-  let symbol = document.getElementById('mySelect').value;
-  // let symbol = options[selectedIndex].id;
-  console.log(symbol);
-};
 
-// function cone_width(height){
-//     return parseInt(2*height-1);
-//   };
-// function make_cone(){
-//   // userInput = document.getElementById.value
-// };  
-//   let width, oWidth;
-//   width = oWidth = cone_width(5);
-//   let firstLine = '';
-  
-//   for(let num=oWidth; num > 0; num--){
-//     if(width == oWidth){
-//       for(let num=width; num > 0; num--){
-//         firstLine += '*';
-//       };
-//       console.log(firstLine+ '\n');
-//       width--;
-//     }else{
-//       if(width % 2 === 1){
-//         let nextLine = '';
-//         let spaces = parseInt((oWidth - width)/2);
-//         for(let num=spaces; num>0; num--){
-//             nextLine += '1';
+
+function cone_width(height){
+    return parseInt(2*height-1);
+  };
+
+
+function make_cone(){
+  userInput = document.getElementById('height-tri').value;
+  let width, oWidth;
+  width = oWidth = cone_width(userInput);
+  let firstLine = '';
+  let symbol = document.getElementById('mySelect').value;
+  for(let num=oWidth; num > 0; num--){
+    if(width == oWidth){
+      for(let num=width; num > 0; num--){
+        firstLine += symbol;
+      };
+      console.log(firstLine+ '\n');
+      width--;
+    }else{
+      if(width % 2 === 1){
+        let nextLine = '';
+        let spaces = parseInt((oWidth - width)/2);
+        for(let num=spaces; num>0; num--){
+            nextLine += ' ';
           
-//         };
-//         for(let num=width; num > 0; num--){
-//             nextLine += '*';
-//         };
-//         console.log(nextLine+'\n');
-//         width--;
-//       }else{
-//           width--;
-//       };
-//     };
-//   };
+        };
+        for(let num=width; num > 0; num--){
+            nextLine += symbol;
+        };
+        console.log(nextLine+'\n');
+        width--;
+      }else{
+          width--;
+      };
+    };
+  };
+};  
+  
 
 
 
