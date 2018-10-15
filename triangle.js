657fe2b22e531306ff5718d8561b98b37dbd4517
mkdir-helen <experiment.helen@gmail.com>
2018-10-15 14:16:25 -0400

    Added the square function to work on the page

--- triangle.js
+++ triangle.js
@@ -1,19 +1,43 @@
 //I want to build a cone in javascript
+let symbol = document.getElementById('mySelect').value;
+let answer = document.getElementById("showcase");
 
 // outputElement.textContent = newFortune;
-// const triangle = document.querySelector('[data-shape="triangle"]');
+const triangle = document.querySelector('[data-shape="triangle"]');
 const cone = document.querySelector('[data-shape="cone"]');
-// const box = document.querySelector('[data-shape="box"]');
-// const square = document.querySelector('[data-shape="square"]');
+const box = document.querySelector('[data-shape="box"]');
+const square = document.querySelector('[data-shape="square"]');
 
 
+//Changing the DOM when the user chooses an option
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('select[name="shapes"]').onchange=changeEventHandler;
+}, false);
 
+function changeEventHandler(event){
+  if(event.target.value === "square"){
+    let dataShape = document.getElementById('submit');
+    dataShape.setAttribute("data-shape", "square");
+    document.getElementById('height-tri-label').innerHTML = "Enter the size of the square:";
+    dataShape.addEventListener('click', make_square);
+  };
+}
+
+
+function make_square (){
+  let num = document.getElementById('height-tri').value;
+  for(let row=0;row<num; row++){
+    let line = '';
+    for(let col=0; col<num; col++){
+        line += symbol;
+    };
+    answer.innerHTML += line + '<br>';
+  };
+}
 
 //Calling the cone function
 cone.addEventListener('click', make_cone);
 
-let symbol = document.getElementById('mySelect').value;
-let answer = document.getElementById("showcase");
 
 ////////////CONE//////////////////////////
 function cone_width(height){
